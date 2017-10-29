862bc75b74892074101b8d53e89e46b6a569b3d5
Thomas Poulet <1039134+Lord-Nazdar@users.noreply.github.com>
2017-10-29 19:40:16 +0100

    Allow for CORS

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_httpauth import HTTPBasicAuth
 from flask_restful import Api
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 auth = HTTPBasicAuth()
 api = Api(app)
+
+CORS(app)
 
 from app import models, users, guide
