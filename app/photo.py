996faf7955cc5d998f27ce0191b4920e4d86e385
Thomas Poulet <1039134+Lord-Nazdar@users.noreply.github.com>
2017-11-13 18:44:27 +0100

    derp ...

--- app/photo.py
+++ app/photo.py
@@ -120,7 +120,7 @@ class PhotoSelect_API(Resource):
                 if matching != []:
                     print(matching[0]['raw']['_content'])
                     matched = re.search( r'(\d{1,4}(?:\.0)?)(?:-(\d{1,4}(?:\.0)?))? ?mm', matching[0]['raw']['_content'], re.M|re.I)
-                    if matched.groups(2) != None:
+                    if matched.groups(2) == None:
                         lens_focal = matched.group(1)
                     else:
                         lens_focal = matched.group(1) + " " + matched.group(2)
